40e2c9a181e8d6f101b97f593fda900e2972d2f1
sky9023 <25349023.qq@gmail.com>
2022-09-11 15:41:10 +0800

    [UPDATE] chrome extension

---
+++ chrome_extension/content-script.js
@@ -0,0 +1,25 @@
+function get_src(e) {
+    let img = document.querySelector(".input_box + img");
+    let components = img["src"].split("/");
+    return components[components.length - 1];
+}
+
+const BASE_URL = "https://7s7izxcb32.execute-api.us-east-1.amazonaws.com/decaptcha";
+
+function decaptcha(e) {
+    let src = get_src(e);
+    let input_box = document.querySelector(".input_box[name=passwd2]");
+    console.log(input_box);
+
+    fetch(BASE_URL, {
+        body: JSON.stringify({src: src}),
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "POST",
+    })
+        .then(resp => resp.json())
+        .then(json => input_box["value"] = json['answer']);
+}
+
+addEventListener('load', decaptcha)
