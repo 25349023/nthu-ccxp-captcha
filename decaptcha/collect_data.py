74fb1303baeaaa50f5f61674a55cf95e443a8d05
sky9023 <25349023.qq@gmail.com>
2022-09-13 20:36:41 +0800

    [UPDATE] improve performance with session

--- decaptcha/collect_data.py
+++ decaptcha/collect_data.py
@@ -8,15 +8,15 @@ from bs4 import BeautifulSoup
 BASE_URL = 'https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/'
 
 
-def get_img_src() -> str:
-    res = requests.get(BASE_URL)
+def get_img_src(session: requests.Session) -> str:
+    res = session.get(BASE_URL)
     soup = BeautifulSoup(res.text, 'lxml')
     img = soup.select_one('.input_box + img')
     return img['src']
 
 
-def manually_label(src: str) -> str:
-    res = requests.get(BASE_URL + src)
+def manually_label(src: str, session: requests.Session) -> str:
+    res = session.get(BASE_URL + src)
     f = io.BytesIO(res.content)
     img = plt.imread(f)
     plt.imshow(img)
@@ -25,19 +25,20 @@ def manually_label(src: str) -> str:
     return label
 
 
-def collect_one(save_dir: Path, generate_count: int):
-    src = get_img_src()
-    file_prefix = manually_label(src)
+def collect_one(save_dir: Path, generate_count: int, session: requests.Session):
+    src = get_img_src(session)
+    file_prefix = manually_label(src, session)
 
     for i in range(generate_count):
-        res = requests.get(BASE_URL + src)
+        res = session.get(BASE_URL + src)
         with open(save_dir / f'{file_prefix}_{i}.png', 'wb') as f:
             f.write(res.content)
 
 
 def collect_many(save_dir: Path, n_round: int, cnt_per_round: int):
+    sess = requests.Session()
     for r in range(n_round):
-        collect_one(save_dir, cnt_per_round)
+        collect_one(save_dir, cnt_per_round, sess)
 
 
 if __name__ == '__main__':
